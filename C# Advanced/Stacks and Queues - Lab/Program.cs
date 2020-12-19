4ce74ea7d7cf73d76ceae989f8e1b81507b94fde
Svetoslav Milanov <svet.milanov@gmail.com>
2020-12-19 18:37:37 +0200

    Add files via upload

---
+++ C# Advanced/Stacks and Queues - Lab/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+
+
+namespace Supermarket
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Queue<string> supermarket = new Queue<string>();
+            
+            while (true)
+            {
+                string input = Console.ReadLine();
+
+                if (input == "End")
+                {
+                    break;
+                }
+                else if (input =="Paid")
+                {
+                    while (supermarket.Count>0)
+                    {
+                        Console.WriteLine(supermarket.Dequeue());
+                    }
+                }
+                else
+                {
+                    supermarket.Enqueue(input);
+                }
+            }
+
+            Console.WriteLine($"{supermarket.Count} people remaining.");
+        }
+    }
+}
